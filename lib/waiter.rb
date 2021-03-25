d5f4f7a52f4002007bc8def8e08bd534519903c3
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-03-24 17:45:33 -0700

    commit tree fcf8ffd181e3f63b10d593159be014e61b3bda9d

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -29,7 +29,6 @@ class Waiter
         best_tipped_meal = meals.max do |meal_a, meal_b|
           meal_a.tip <=> meal_b.tip  #look up this code
         end
-      
         best_tipped_meal.customer
     end 
 end
